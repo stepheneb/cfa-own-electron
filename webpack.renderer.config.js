0642d6a677d7dff8ccd7787f27553a70fbb9e99c
Stephen Bannasch <stephen.bannasch@gmail.com>
2021-08-11 18:41:41 -0400

    copy index.html redirect to ./webpack/renderer
    The content in ./webpack/renderer is also committed to the gh-pages
    branch to create the static website:
    
    https://stepheneb.github.io/cfa-own-electron
    
    The static site is actually available at:
    https://stepheneb.github.io/cfa-own-electron/main_window/index.html
    
    This index.html redirect is copied to the root of ./webpack/renderer
    so a request for:
    https://stepheneb.github.io/cfa-own-electron
    
    redirects to:
    https://stepheneb.github.io/cfa-own-electron/main_window/index.html
    
    This file has no effect on the electron application.

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -7,6 +7,18 @@ const path = require('path');
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const assets = ['images', 'rawdata', 'fonts'];
 
+let patterns = assets.map(asset => {
+  return {
+    from: path.resolve(__dirname, 'src', asset),
+    to: path.resolve(__dirname, '.webpack/renderer', asset)
+  };
+});
+patterns.push({
+  from: path.resolve(__dirname, 'src/index.html'),
+  to: path.resolve(__dirname, '.webpack/renderer/index.html')
+
+});
+
 module.exports = {
   // Put your normal webpack config below here
   module: {
@@ -17,12 +29,7 @@ module.exports = {
   },
   plugins: [
     new CopyWebpackPlugin({
-      patterns: assets.map(asset => {
-        return {
-          from: path.resolve(__dirname, 'src', asset),
-          to: path.resolve(__dirname, '.webpack/renderer', asset)
-        };
-      })
+      patterns: patterns
     }),
     new MiniCssExtractPlugin({
       filename: isDevelopment ? '[name].css' : '[name].[hash].css',
