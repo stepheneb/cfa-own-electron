297d223b1f1c6f5b3e28f607ef411145b95e1b4c
Stephen Bannasch <stephen.bannasch@gmail.com>
2021-09-27 13:10:21 -0400

    utilities: add set operations

--- src/renderer/modules/utilities.js
+++ src/renderer/modules/utilities.js
@@ -129,4 +129,54 @@ utilities.UUID = (function () {
   return self;
 })();
 
+// Set Utilities
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+
+utilities.setIsSuperset = (set, subset) => {
+  for (let elem of subset) {
+    if (!set.has(elem)) {
+      return false;
+    }
+  }
+  return true;
+};
+
+utilities.setUnion = (setA, setB) => {
+  let _union = new Set(setA);
+  for (let elem of setB) {
+    _union.add(elem);
+  }
+  return _union;
+};
+
+utilities.setIntersection = (setA, setB) => {
+  let _intersection = new Set();
+  for (let elem of setB) {
+    if (setA.has(elem)) {
+      _intersection.add(elem);
+    }
+  }
+  return _intersection;
+};
+
+utilities.setSymmetricDifference = (setA, setB) => {
+  let _difference = new Set(setA);
+  for (let elem of setB) {
+    if (_difference.has(elem)) {
+      _difference.delete(elem);
+    } else {
+      _difference.add(elem);
+    }
+  }
+  return _difference;
+};
+
+utilities.setDifference = (setA, setB) => {
+  let _difference = new Set(setA);
+  for (let elem of setB) {
+    _difference.delete(elem);
+  }
+  return _difference;
+};
+
 export default utilities;
