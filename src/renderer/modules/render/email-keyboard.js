acd44a365d587393ba8d15f9976c1fd001b53036
Stephen Bannasch <stephen.bannasch@gmail.com>
2021-10-12 10:59:43 -0400

    keyboard styling: wip

--- src/renderer/modules/render/email-keyboard.js
+++ src/renderer/modules/render/email-keyboard.js
@@ -44,17 +44,24 @@ emailKeyboard.render = (page, sendEmailFormId, registeredCallbacks) => {
           '1 2 3 4 5 6 7 8 9 0 - {bksp}',
           'q w e r t y u i o p',
           'a s d f g h j k l',
-          '{shift} z x c v b n m . {shift}',
-          '@ .com'
+          '{shift} z x c v b n m {shift}',
+          '. @ .com'
         ],
         'shift': [
-          '` ~ ! @ # $ % ^ & * _ + {bksp}',
+          '` ~ ! # $ % ^ & * _ + {bksp}',
           'Q W E R T Y U I O P',
-          'A S D F G H J K L :',
+          'A S D F G H J K L',
           '{shift} Z X C V B N M {shift}',
-          '.com @'
+          '. @ .com'
         ]
-      }
+      },
+      buttonTheme: [{
+        class: 'top-row',
+        buttons: '1 2 3 4 5 6 7 8 9 0 - ` ~ ! # $ % ^ & * _ + '
+      }, {
+        class: 'bottom-row',
+        buttons: '@ .com'
+      }]
     });
 
     function checkEmail(input) {
