ac79ef34bf19145065202b538f32adec5b5ef428
Stephen Bannasch <stephen.bannasch@gmail.com>
2021-10-29 12:53:23 -0400

    admin: Test CfA Handshake button

--- src/renderer/admin.js
+++ src/renderer/admin.js
@@ -14,6 +14,15 @@ const enterCfaKey = document.getElementById('enter-cfa-key');
 const newCfaKey = document.getElementById('new-cfa-key');
 
 if (u.runningInElectron()) {
+
+  const cfaKeyHasValidFormat = () => {
+    let cfa_key = app.kioskState.cfa_key;
+    if (cfa_key === undefined) return false;
+    if (typeof cfa_key !== 'string') return false;
+    if (cfa_key.length !== 17) return false;
+    return true;
+  };
+
   restart.addEventListener('click', () => {
     console.log("admin:restart button pressed.");
     ipcRenderer.send('restart');
@@ -33,6 +42,9 @@ if (u.runningInElectron()) {
     } else {
       cfa_key_elem.innerText = 'not set';
     }
+    if (cfaKeyHasValidFormat()) {
+      testCfaHandshake.disabled = false;
+    }
   });
 
   enterCfaKey.onsubmit = async () => {
@@ -40,7 +52,46 @@ if (u.runningInElectron()) {
     ipcRenderer.invoke('new-cfa-key', obj).then((kioskState) => {
       app.kioskState = kioskState;
     });
+    if (cfaKeyHasValidFormat()) {
+      testCfaHandshake.disabled = false;
+    }
   };
+
+  const testCfaHandshake = document.getElementById('test-cfa-handshake');
+
+  testCfaHandshake.addEventListener('click', () => {
+    const cfaHandshakePostUrl = "https://waps.cfa.harvard.edu/microobservatory/own_kiosk/api/v1/handshake/handshake.php";
+    const cfaHandshakeRequest = document.querySelector('#cfa-handshake-status .request');
+    const cfaHandshakeReponse = document.querySelector('#cfa-handshake-status .response');
+
+    let request = {
+      kiosk_id: app.kioskState.id,
+      credential: app.kioskState.cfa_key
+    };
+    let response = '';
+
+    cfaHandshakeRequest.innerText = JSON.stringify(request, null, '  ');
+    cfaHandshakeReponse.innerText = response;
+
+    fetch(cfaHandshakePostUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        },
+        body: JSON.stringify(request)
+      })
+      .then(response => response.json())
+      .then(json => {
+        response = JSON.stringify(json, null, '  ');
+        console.log(response);
+        cfaHandshakeReponse.innerText = response;
+      })
+      .catch(error => {
+        response = `Request to send image failed: ${error}.\nThe Developer Tools console might have more clues.`;
+        console.error(response);
+        cfaHandshakeReponse.innerText = response;
+      });
+  });
 }
 
 export default admin;
