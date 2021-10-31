6850aa9305a504526b3cab37ab710f40f21569f1
Stephen Bannasch <stephen.bannasch@gmail.com>
2021-10-30 21:11:10 -0400

    admin: styling, layout, erase-cfa-key button

--- src/renderer/admin.js
+++ src/renderer/admin.js
@@ -8,10 +8,19 @@ let admin = {};
 
 const restart = document.getElementById('restart');
 const quit = document.getElementById('quit');
-const cfa_key_elem = document.getElementById('cfa-key-id');
+
+const needCfaKey = document.querySelector('#content .need-cfa-key');
+
+const cfaKeyElem = document.getElementById('cfa-key-id');
 
 const enterCfaKey = document.getElementById('enter-cfa-key');
 const newCfaKey = document.getElementById('new-cfa-key');
+
+const eraseCfaKey = document.getElementById('erase-cfa-key');
+// JXRv3cwNOEnDhz8H8
+
+const cfaHandshakeRequest = document.querySelector('#cfa-handshake-status .request');
+const cfaHandshakeReponse = document.querySelector('#cfa-handshake-status .response');
 
 if (u.runningInElectron()) {
 
@@ -21,6 +30,30 @@ if (u.runningInElectron()) {
     if (typeof cfa_key !== 'string') return false;
     if (cfa_key.length !== 17) return false;
     return true;
+  };
+
+  const updateView = () => {
+    let kiosk_elem = document.getElementById('kiosk-id');
+    kiosk_elem.innerText = app.kioskState.id;
+    if (app.kioskState.cfa_key) {
+      cfaKeyElem.innerText = app.kioskState.cfa_key;
+    } else {
+      cfaKeyElem.innerText = 'not set';
+    }
+    if (app.kioskState.cfa_key) {
+      eraseCfaKey.disabled = false;
+    } else {
+      eraseCfaKey.disabled = true;
+    }
+    if (cfaKeyHasValidFormat()) {
+      testCfaHandshake.disabled = false;
+      needCfaKey.classList.remove('show');
+    } else {
+      testCfaHandshake.disabled = true;
+      needCfaKey.classList.add('show');
+    }
+    cfaHandshakeRequest.innerText = '';
+    cfaHandshakeReponse.innerText = '';
   };
 
   restart.addEventListener('click', () => {
@@ -35,16 +68,7 @@ if (u.runningInElectron()) {
 
   ipcRenderer.invoke('getKioskState').then((kioskState) => {
     app.kioskState = kioskState;
-    let kiosk_elem = document.getElementById('kiosk-id');
-    kiosk_elem.innerText = kioskState.id;
-    if (kioskState.cfa_key) {
-      cfa_key_elem.innerText = kioskState.cfa_key;
-    } else {
-      cfa_key_elem.innerText = 'not set';
-    }
-    if (cfaKeyHasValidFormat()) {
-      testCfaHandshake.disabled = false;
-    }
+    updateView();
   });
 
   enterCfaKey.onsubmit = async () => {
@@ -52,17 +76,21 @@ if (u.runningInElectron()) {
     ipcRenderer.invoke('new-cfa-key', obj).then((kioskState) => {
       app.kioskState = kioskState;
     });
-    if (cfaKeyHasValidFormat()) {
-      testCfaHandshake.disabled = false;
-    }
+    updateView();
   };
+
+  eraseCfaKey.addEventListener('click', () => {
+    let obj = { "new-cfa-key": null };
+    ipcRenderer.invoke('new-cfa-key', obj).then((kioskState) => {
+      app.kioskState = kioskState;
+    });
+    updateView();
+  });
 
   const testCfaHandshake = document.getElementById('test-cfa-handshake');
 
   testCfaHandshake.addEventListener('click', () => {
     const cfaHandshakePostUrl = "https://waps.cfa.harvard.edu/microobservatory/own_kiosk/api/v1/handshake/handshake.php";
-    const cfaHandshakeRequest = document.querySelector('#cfa-handshake-status .request');
-    const cfaHandshakeReponse = document.querySelector('#cfa-handshake-status .response');
 
     let request = {
       kiosk_id: app.kioskState.id,
@@ -87,7 +115,7 @@ if (u.runningInElectron()) {
         cfaHandshakeReponse.innerText = response;
       })
       .catch(error => {
-        response = `Request to perform handshake failed: ${error}.\nThe Developer Tools console might have more clues.`;
+        response = `Request to perform handshake failed: ${error}, the Developer Tools console might have more clues.`;
         console.error(response);
         cfaHandshakeReponse.innerText = response;
       });
