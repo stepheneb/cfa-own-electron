2aa860a42fa455e807d917d91f4f01980d01cfff
stepheneb <stepheneb@users.noreply.github.com>
2022-01-20 06:45:10 +0000

    deploy: f6eb28a0ee8d5f363c96f0ad4f18d678cb762896

--- admin_window/preload.js
+++ admin_window/preload.js
@@ -1,2 +1,2 @@
-(()=>{var e={298:e=>{"use strict";e.exports=require("electron")}},r={};function o(t){var n=r[t];if(void 0!==n)return n.exports;var i=r[t]={exports:{}};return e[t](i,i.exports,o),i.exports}(()=>{const{ipcRenderer:e,contextBridge:r}=o(298);r.exposeInMainWorld("ELECTRON",!0),r.exposeInMainWorld("ipcRenderer",e)})()})();
+(()=>{var e={298:e=>{"use strict";e.exports=require("electron")}},r={};function o(t){var n=r[t];if(void 0!==n)return n.exports;var a=r[t]={exports:{}};return e[t](a,a.exports,o),a.exports}(()=>{const{ipcRenderer:e,contextBridge:r}=o(298);r.exposeInMainWorld("ELECTRON",!0),r.exposeInMainWorld("ipcRenderer",e),r.exposeInMainWorld("api",{logDataUpdate:r=>{e.on("logDataUpdate",((e,...o)=>r(...o)))}})})()})();
 //# sourceMappingURL=preload.js.map
